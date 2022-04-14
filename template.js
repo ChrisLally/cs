328bd3308da74b907e06a86fcf95e28ac97c63a2
Chris <24978693+clally1@users.noreply.github.com>
2022-04-14 10:30:54 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,54 @@
+   <main class="main-wrapper">
+    <section class="banner-section" style="
+     background-image: url('{{{bannerImage}}}');
+   "></section>
+   
+    <section class="content-section">
+       <div class="page-padding left">
+          <div class="top-div">
+             <img src="{{{image}}}" loading="lazy" alt="" class="project-logo"><img src="{{{image}}}" loading="lazy" alt="" class="project-logo">
+             <div class="div-block-3">
+                <div class="div-block-2">
+                   <h1 class="heading-3">{{{name}}}</h1>
+                   <a href="#" class="cid w-inline-block">
+                      <div>CID:{{{id}}}</div>
+                   </a>
+                   <div>
+       							{{#contributors.organizations}}
+                        <a href="https://chainsight.webflow.io/cid?CID={{cid}}" class="cid-link w-inline-block">
+                      <img src="{{image}}" loading="lazy" alt="" class="cid-link-image">
+                      <div class="cid-link-text-div">
+                         <div class="cid-link-name">Name: {{name}}</div>
+                         <div class="cid-link-title">Title: {{role}}</div>
+                      </div>
+                   </a>
+                       </div>
+       							{{/contributors.organizations}}
+     							
+                  <h4>{{{shortDescription}}}</h4>
+
+                  </div>
+                  
+                </div>
+             </div>
+          <div class="w-layout-grid social-grid">
+   {{#if websiteUrl}}
+   <a href="{{{websiteUrl}}}" class="social-link website w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62214815305a76470cc75f89_icons8-website-50.png" loading="lazy" alt="" class="social-logo"></a>
+   {{/if}}
+   <a href="#" class="social-link discord w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62212758116b2eff7106cbc8_icons8-discord-new%20(1).svg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link twitter w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/6221479ba1d5c043416ea5eb_icons8-twitter-48.png" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link medium w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/622148dd9b43f64e664874a5_icons8-medium-monogram%20(1).svg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link mirror w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62214a6f305a76be61c77a08_VVWXmSVk_400x400.jpeg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link linkedin w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62214b172c4802fb23eb0c0f_icons8-linkedin.svg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link wikipedia w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62214b7360b945221ff913b4_icons8-wikipedia.svg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link opensea w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62214be1a63e4030ced0c7c2_Logomark-Transparent%20White.svg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link telegram w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62214c43278432b001d7b25f_icons8-telegram-app.svg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link instagram w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/6221588df8963a80cedce468_icons8-instagram.svg" loading="lazy" alt="" class="social-logo"></a>
+   <a href="#" class="social-link youtube w-inline-block"><img src="https://uploads-ssl.webflow.com/620fe616062cf52d86658553/62215b2ac30e097953c33ab6_icons8-youtube.svg" loading="lazy" alt="" class="social-logo"></a>
+   </div>
+       
+       </div>
+       <div data-current="Opportunities" data-easing="ease" data-duration-in="300" data-duration-out="100" class="cid-tabs w-tabs"><div class="tabs-menu-2 w-tab-menu" role="tablist"><a data-w-tab="Opportunities" class="tab w-inline-block w-tab-link w--current" id="w-tabs-8-data-w-tab-0" href="#w-tabs-8-data-w-pane-0" role="tab" aria-controls="w-tabs-8-data-w-pane-0" aria-selected="true"><div>Opportunities</div></a><a data-w-tab="Audits" class="tab w-inline-block w-tab-link" tabindex="-1" id="w-tabs-8-data-w-tab-1" href="#w-tabs-8-data-w-pane-1" role="tab" aria-controls="w-tabs-8-data-w-pane-1" aria-selected="false"><div>Audits</div></a></div><div class="w-tab-content"><div data-w-tab="Opportunities" class="tab-content w-tab-pane w--tab-active" id="w-tabs-8-data-w-pane-0" role="tabpanel" aria-labelledby="w-tabs-8-data-w-tab-0"><div class="page-padding"><h1 class="heading-2">Coming Soon...</h1><a href="#" class="button large w-button">View Example</a><div id="w-node-d8fc82a1-3972-502b-e94a-bd1a469b0683-decf51b4" class="op-div"><h2 id="w-node-d8fc82a1-3972-502b-e94a-bd1a469b0684-decf51b4" class="heading-title">Opportunities</h2></div></div></div><div data-w-tab="Audits" class="tab-content w-tab-pane" id="w-tabs-8-data-w-pane-1" role="tabpanel" aria-labelledby="w-tabs-8-data-w-tab-1"><div class="page-padding"><div id="w-node-d8fc82a1-3972-502b-e94a-bd1a469b06b5-decf51b4" class="op-div"><h2 id="w-node-d8fc82a1-3972-502b-e94a-bd1a469b06b6-decf51b4" class="heading-title">3rd Party Audits</h2><h4 class="center-heading"><em>Professional audits or reviews will be shared here, none found yet.</em><br></h4></div></div></div></div></div>
+       </div>
+    </section>
+   </main>
